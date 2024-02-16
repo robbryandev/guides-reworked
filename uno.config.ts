34a1944d2347e7656d5573b376e3f0eca32a0b05
robbryandev <robbryandev@gmail.com>
2024-02-16 12:42:24 -0800

    Add navbar

--- uno.config.ts
+++ uno.config.ts
@@ -2,6 +2,17 @@ import { defineConfig, presetWind } from 'unocss';
 import transformerVariantGroup from '@unocss/transformer-variant-group';
 
 export default defineConfig({
+  rules: [
+    ["bg-center", {
+      "background-position": "center"
+    }],
+    ["bg-repeat-none", {
+      "background-repeat": "no-repeat"
+    }],
+    ["bg-img-game", {
+      "background-image": "url('button_gamedev_transparent.png')"
+    }]
+  ],
   presets: [
     presetWind()
   ],
