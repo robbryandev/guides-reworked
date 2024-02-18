64733d71a1e16c4c7d1d5e1d5dac9bede6a3d796
robbryandev <robbryandev@gmail.com>
2024-02-17 16:36:43 -0800

    Add twitch embed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config'
-import UnoCSS from 'unocss/astro'
+import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    UnoCSS()
-  ]
-})
+  integrations: [UnoCSS(), react()]
+});
